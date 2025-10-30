cd3c8858f400cbb129078d579489fad83f047c98
carolylazo <carol.lazo@assuresoft.tech>
2025-10-30 16:54:33 -0400

    chore: TC001 api connection

--- src/tests/TC001.spec.ts
+++ src/tests/TC001.spec.ts
@@ -3,11 +3,18 @@ import { expect } from "../fixtures/LogInPageFixture";
 import { test as loggedInTest } from "../fixtures/LoggedInPageFixture";
 import { test as dashboardPage } from "../fixtures/DashboardFixture";
 import data from "../utils/data/DataTest.json";
-import { url } from "inspector";
+import { ApiClient } from "../utils/api/ApiClient";
+import { config } from "../utils/config/config";
+import { EndPoints } from "../utils/api/endpoints";
 
 const test = mergeTests(loggedInTest, dashboardPage);
 
 test.describe("YOURLS add shorturls", () => {
+  let api: any;
+  test.beforeAll(async () => {
+    api = new ApiClient("http://localhost:8080");
+    await api.init();
+  });
   for (const urls of data) {
     test(`Short ure for ${urls.title} was created`, async ({
       dashboardPage,
@@ -17,6 +24,15 @@ test.describe("YOURLS add shorturls", () => {
       await dashboardPage.fillURL(urls.url);
       await dashboardPage.fillKeyword(urls.keyword);
       await dashboardPage.clickCreateShortUrl();
+      const params = {
+        signature: "caed1384a5",
+        action: "expand",
+        shorturl: urls.keyword,
+        format: "json",
+      };
+      const response = await api.get(EndPoints.yourls.basic, params);
+      const responseData = await response.json();
+      expect(response.status(), "Status code").toBe(200);
     });
   }
 });
